cd84adc8d2978a062b3e511fd1003762426bec65
danijel <danijel.kecman@gmail.com>
2019-04-01 16:23:23 +0200

    - work on core

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test custom creating a new customuser with an email without username is successful"""
+        email = 'test@test.com'
+        password = '123'
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_create_new_user_with_email_normalized(self):
+        """Test that the email for a new user is normalized"""
+        email = 'test@TEST.COM'
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = '123'
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test no email raises email"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, '123')
+
+    def test_create_new_superuser(self):
+        """Test creating superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@test.com',
+            '123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
--- restart-app.sh
+++ restart-app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker-compose down
 docker-compose up -d
-docker-compose run app
+#docker-compose run app
