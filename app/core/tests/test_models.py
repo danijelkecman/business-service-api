54fdfe5ef779bf2734987a8c0572f376f0a89964
danijel <danijel.kecman@gmail.com>
2019-04-02 20:16:52 +0200

    - category strategy

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from .. import models
+
+def sample_user(email='test@test.com', password='12345'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
 
@@ -40,3 +46,13 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_category_str(self):
+        """Test the category string representation"""
+        category = models.Category.objects.create(
+            user=sample_user(),
+            name='IT'
+        )
+        
+        self.assertEqual(str(category), category.name)
+
