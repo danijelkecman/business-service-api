d12f65533e87000f1e81b3e47986c6ed61d17656
danijel <danijel.kecman@gmail.com>
2019-04-07 22:25:27 +0200

    - pillow and dockerfile changes

--- app/app/settings.py
+++ app/app/settings.py
@@ -126,5 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROUTE = '/vol/web/media'
+STATUC_ROUTE = '/vol/web/static'
 
 AUTH_USER_MODEL = 'core.User'
